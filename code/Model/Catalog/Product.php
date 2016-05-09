efee11f14e1d969e7050379d31ae701a4a2d3ac6
Rasmus Bech <me@rbech.com>
2016-05-09 09:00:28 +0200

    Allow users to extend the data exported to Clerk, fix minor spelling errors

--- code/Model/Catalog/Product.php
+++ code/Model/Catalog/Product.php
@@ -22,45 +22,48 @@ class Clerk_Clerk_Model_Catalog_Product extends Clerk_Clerk_Model_Catalog_Produc
      */
     public function getClerkExportData()
     {
-        $data = array();
-        $data['age'] = (int) $this->getAge();
-        $data['categories'] = array_map('intval', $this->getCategoryIds());
-        $data['description'] = $this->getDescription();
-        $data['has_tierprice'] = $this->hasTierPrice();
-        $data['id'] = (int) $this->getId();
-        $data['image'] = $this->getClerkImageUrl();
-        $data['is_on_sale'] = $this->isOnSale();
-        $data['manufacturer'] = $this->getManufacturer();
-        $data['meta_description'] = $this->getMetaDescription();
-        $data['meta_keywords'] = $this->getMetaKeyword();
-        $data['meta_title'] = $this->getMetaTitle();
-        $data['name'] = $this->getName();
-        $data['price'] = $this->getClerkRetailPrice();
-        $data['price_final_excl_tax'] = $this->getClerkFinalPrice();
-        $data['price_final_incl_tax'] = $this->getClerkFinalPriceInclTax();
-        $data['price_retail_excl_tax'] = $this->getClerkRetailPrice();
-        $data['price_retail_incl_tax'] = $this->getClerkRetailPriceInclTax();
-        $data['short_description'] = $this->getShortDescription();
-        $data['sku'] = $this->getSku();
-        $data['url'] = $this->getProductUrl();
-        $data['visibility'] = $this->getVisibility();
+        $data = new Varien_Object();
 
-        return $data;
+        $data->setAge((int) $this->getAge());
+        $data->setCategories(array_map('intval', $this->getCategoryIds()));
+        $data->setDescription($this->getDescription());
+        $data->setHasTierprice($this->hasTierPrice());
+        $data->setId((int) $this->getId());
+        $data->setImage($this->getClerkImageUrl());
+        $data->setIsOnSale($this->isOnSale());
+        $data->setManufacturer($this->getManufacturer());
+        $data->setMetaDescription($this->getMetaDescription());
+        $data->setMetaKeywords($this->getMetaKeyword());
+        $data->setMetaTitle($this->getMetaTitle());
+        $data->setName($this->getName());
+        $data->setPrice($this->getClerkRetailPrice());
+        $data->setPriceFinalExclTax($this->getClerkFinalPrice());
+        $data->setPriceFinalInclTax($this->getClerkFinalPriceInclTax());
+        $data->setPriceRetailExclTax($this->getClerkRetailPrice());
+        $data->setPriceRetailInclTax($this->getClerkRetailPriceInclTax());
+        $data->setShortDescription($this->getShortDescription());
+        $data->setSku($this->getSku());
+        $data->setUrl($this->getProductUrl());
+        $data->setVisibility($this->getVisibility());
+
+        Mage::dispatchEvent('clerk_get_export_data', array('product' => $this, 'data' => $data));
+
+        return $data->toArray();
     }
 
     /**
-     * Returns a boolean indicating weather a product should be included in
+     * Returns a boolean indicating whether a product should be included in
      * data export to Clerk. Thus, developers can use this function to filter
      * products exportet to clerk.
      *
      * NOTE: Please set $this->excludeReason as shown in the example below if
-     * a product should be excluded, this will help debuggin future errors
+     * a product should be excluded, this will help debugging future errors
      * where products are not showing up correctly on the website.
      */
     public function setExcludeReason()
     {
         if ($this->getVisibility() == '1') {
-            $this->excludeReason = 'Product not visable';
+            $this->excludeReason = 'Product not visible';
         }
 
         if (!$this->isSalable()) {
