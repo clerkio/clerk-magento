350949b0150316201781401c9f22276a2db6e46f
Rasmus Bech <me@rbech.com>
2017-05-26 13:38:06 +0200

    Change default template names to match clerk.io standards, change logic to get import url - closes #36, closes #41

--- code/Block/Adminhtml/Url.php
+++ code/Block/Adminhtml/Url.php
@@ -2,12 +2,18 @@
 
 class Clerk_Clerk_Block_Adminhtml_Url extends Mage_Adminhtml_Block_System_Config_Form_Field
 {
+    /**
+     * Get store import URL
+     *
+     * @param Varien_Data_Form_Element_Abstract $element
+     * @return string
+     */
     protected function _getElementHtml(Varien_Data_Form_Element_Abstract $element)
     {
         $code = Mage::getSingleton('adminhtml/config_data')->getStore();
-        $code = Mage::getModel('core/store')->load($code)->getId();
-        $url = (string) Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_WEB).'clerk/api/store/'.$code;
+        $store = Mage::getModel('core/store')->load($code);
+        $url = (string) $store->getBaseUrl(Mage_Core_Model_Store::URL_TYPE_WEB).'clerk/api/store/'.$store->getId();
 
-        return "<a href=\"{$url}\">{$url}</a>";
+        return sprintf('<a href="%1$s" target="_blank">%1$s</a>', $url);
     }
 }
--- code/Block/Adminhtml/Version.php
+++ code/Block/Adminhtml/Version.php
@@ -2,6 +2,12 @@
 
 class Clerk_Clerk_Block_Adminhtml_Version extends Mage_Adminhtml_Block_System_Config_Form_Field
 {
+    /**
+     * Get Clerk extension version
+     *
+     * @param Varien_Data_Form_Element_Abstract $element
+     * @return string
+     */
     protected function _getElementHtml(Varien_Data_Form_Element_Abstract $element)
     {
         return (string) Mage::getConfig()->getNode()->modules->Clerk_Clerk->version;
