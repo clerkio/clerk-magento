d86efdc0bb948a356daf298daada103292fdab2d
Stubbe <abs@clerk.io>
2024-01-02 14:18:26 +0100

    changed url to custom

--- code/Block/Tracking.php
+++ code/Block/Tracking.php
@@ -2,8 +2,8 @@
 
 class Clerk_Clerk_Block_Tracking extends Mage_Core_Block_Template
 {
-    const XML_PATH_PUBLIC_KEY = 'clerk/general/publicapikey';
-    const XML_PATH_COLLECT_EMAILS = 'clerk/general/collect_emails';
+    public const XML_PATH_PUBLIC_KEY = 'clerk/general/publicapikey';
+    public const XML_PATH_COLLECT_EMAILS = 'clerk/general/collect_emails';
 
     /**
      * Get public key
@@ -33,5 +33,20 @@ class Clerk_Clerk_Block_Tracking extends Mage_Core_Block_Template
     public function collectEmails()
     {
         return Mage::getStoreConfigFlag(self::XML_PATH_COLLECT_EMAILS);
+    }
+
+    /**
+     * Get store name
+     */
+    public function getStoreName()
+    {
+        return Mage::app()->getStore()->getName();
+    }
+
+    public function getClerkJSUrl()
+    {
+        $storeName = $this->getStoreName();
+        $storeName = preg_replace('/[^a-z]/', '', strtolower($storeName));
+        return '://custom.clerk.io/' . $storeName . '.js';
     }
 }
